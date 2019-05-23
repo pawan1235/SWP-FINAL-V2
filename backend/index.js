9de2bdaed89f3fc015277f9c986026b91f8ec076
Pawan Intawongsa <pawan1235@hotmail.com>
2019-05-23 18:24:22 +0700

    first commit

--- backend/index.js
+++ backend/index.js
@@ -12,20 +12,7 @@ var T = new Twit({
   consumer_secret: process.env.API_SECRET_KEY,
   access_token: process.env.TOKEN_KEY,
   access_token_secret: process.env.TOKEN_SECRET_KEY
-  // timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-  // strictSSL: true // optional - requires SSL certificates to be valid.
 });
-
-// var firebaseConfig = {
-//   apiKey: "AIzaSyCE6-PAF2G9pCoapYm5ANPovtbiIjl2Mgo",
-//   authDomain: "swp-final-exam-49609.firebaseapp.com",
-//   databaseURL: "https://swp-final-exam-49609.firebaseio.com",
-//   projectId: "swp-final-exam-49609",
-//   storageBucket: "swp-final-exam-49609.appspot.com",
-//   messagingSenderId: "70623787007",
-//   appId: "1:70623787007:web:72446ab1245061cb"
-// };
-// firebase.initializeApp(firebaseConfig);
 
 const port = process.env.PORT || "4000";
 
@@ -41,15 +28,6 @@ io.on("connection", client => {
   });
 });
 
-// app.get('/', (req, res) => {
-//     var params = {screen_name: 'nodejs'};
-//     client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//         if (!error) {
-//             res.send(tweets);
-//         }
-//     });
-// })
-
 server.get("/", (re1, res) => {
   res.send("Collecting data..");
 });
@@ -62,7 +40,6 @@ let data = {
   count: 0
 };
 stream.on("tweet", async function(tweet) {
-  // console.log(tweet);
   if (
     moment()
       .startOf("minute")
